cfacf5baf44c0e0469b41690a157f8371aefab34
Nicolas Mamet <nmamet@a-018818.ups.u-psud.fr>
2019-03-28 19:28:52 +0100

    Cell is empty and field has 2 constructors for field generation and creating a field from a json object

---
+++ field.js
@@ -0,0 +1,37 @@
+"use strict";
+
+function defined(x){
+    if(typeof x === 'undefined'){
+        throw "Terrain mal formaté";
+    }
+    return x;
+}
+
+class Field extends Object{
+
+    constructor(json_object){
+        super();
+
+        Object.defineProperty(this, "widthCell", {value : defined(json_object.widthCell), writable : false});
+        Object.defineProperty(this, "heightCell", {value : defined(json_object.heightCell), writable : false});
+        Object.defineProperty(this, "nbRow", {value : defined(json_object.nbLine), writable : false});
+        Object.defineProperty(this, "nbCol", {value : defined(json_object.nbCol), writable : false});
+        //Object.defineProperty(this, "cells", {value : defined(json_object.cells), writable : false});
+
+        Object.seal(this);
+    }
+
+    constructor(widthCell,heightCell,nbRow,nbCol){
+        super();
+
+        Object.defineProperty(this, "widthCell", {value : widthCell, writable : false});
+        Object.defineProperty(this, "heightCell", {value : heightCell, writable : false});
+        Object.defineProperty(this, "nbLine", {value : nbRow, writable : false});
+        Object.defineProperty(this, "nbCol", {value : nbCol, writable : false});
+        //Object.defineProperty(this, "cells", {value : defined(json_object.cells), writable : false});
+
+        Object.seal(this);
+    }
+
+    checkPos(r,c){} //true if the cell is crossable
+}
